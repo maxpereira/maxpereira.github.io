c83cc80e701f62ec49ba3bd1cf05689bf233ec2c
Max Pereira <max@pereira.at>
2023-07-30 15:50:25 -0700

    If an iOS device is detected the content width will now expand

--- common.js
+++ common.js
@@ -7,9 +7,23 @@ function toggleVis(elem) {
     }
 }
 
+// iOS device detection (thanks to Paul R. and Pierre https://stackoverflow.com/questions/9038625/detect-if-device-is-ios)
+function iOS() {
+    return [
+      'iPad Simulator',
+      'iPhone Simulator',
+      'iPod Simulator',
+      'iPad',
+      'iPhone',
+      'iPod'
+    ].includes(navigator.platform)
+    // iPad on iOS 13 detection
+    || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+}
+
 // If the screen width is too narrow (like on a mobile device) expand the content to fill the window
 function resizeContent() {
-    if (window.innerWidth <= 900) {
+    if (window.innerWidth <= 900 || iOS()) {
         document.getElementById("bigbox").style.width = "100%";
     } else if (window.innerWidth > 900) {
         document.getElementById("bigbox").style.width = "50%";
